66dabec858cec8919d2614141edd3e064c23f225
Vperez25 <vperez_22@alu.uabcs.mx>
2024-10-14 14:35:28 -0700

    gfgfgf

--- api/cart.js
+++ api/cart.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product');  // Import Mongoose Product model
+const Product = require('/models/Product');  // Import Mongoose Product model
 
 // Get cart items
 router.get('/', async (req, res) => {
