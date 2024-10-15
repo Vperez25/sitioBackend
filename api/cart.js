14567abb73fbea347689657e7c16326794ef0d0b
Vperez25 <vperez_22@alu.uabcs.mx>
2024-10-15 09:32:16 -0700

    gfgfgf

--- api/cart.js
+++ api/cart.js
@@ -5,19 +5,25 @@ const CartItem = require('../models/CartItem');
 
 router.post('/', async (req, res) => {
     const { name, price } = req.body;
-    console.log(name, price);
+
     try {
 
+        let existingItem = await CartItem.findOne({ name });
 
+        if (existingItem) {
+
+            existingItem.quantity += 1;
+            await existingItem.save();
+        } else {
 
             const newItem = new CartItem({ name, price, quantity: 1 });
             await newItem.save();
-
+        }
 
         res.status(200).json({ message: `${name} added to the cart!` });
     } catch (error) {
         console.error('Error adding to cart:', error);
-        res.status(500).json({ error: 'Failed to add item to cart' });
+        res.status(400).json({ error: 'Failed to add item to cart' });
     }
 });
 
