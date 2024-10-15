211d9a26a1e770109acd61013c86a5849c9b14b3
Vperez25 <vperez_22@alu.uabcs.mx>
2024-10-15 08:58:44 -0700

    gfgfgf

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cartRoutes = require('../api/cart');
+const cartRoutes = require('/api/cart');
 
 dotenv.config();
 
